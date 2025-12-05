52c9a8e2bf7cc1a381e7245d6d38c30941890c1c
Cody Williams <williams.c@gmail.com>
2025-12-05 14:50:32 -0600

    Day 5

--- tests/aockt/y2025/Y2025D04Test.kt
+++ tests/aockt/y2025/Y2025D04Test.kt
@@ -3,7 +3,7 @@ package aockt.y2025
 import io.github.jadarma.aockt.test.AdventDay
 import io.github.jadarma.aockt.test.AdventSpec
 
-@AdventDay(2025, 4, "")
+@AdventDay(2025, 4, "Printing Department")
 class Y2025D04Test: AdventSpec<Y2025D04>({
     partOne() {
         """
